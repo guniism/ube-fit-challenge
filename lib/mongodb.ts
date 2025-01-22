284abcf23098e051ee9feb95ccd049ac315681ba
guniism <gun16110@hotmail.com>
2025-01-23 02:47:10 +0700

    Moongoose

---
+++ lib/mongodb.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+export const connectMongoDb = async () => {
+    try {
+        const mongoUri = process.env.MONGODB_URI;
+        if (!mongoUri) {
+            throw new Error("MONGODB_URI is not set");
+        }
+
+        await mongoose.connect(mongoUri);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.log("Error connecting to MongoDB: ", error);
+    }
+}
