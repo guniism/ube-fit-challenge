e5ccf1d4971b0b14aeb2c646f0fc6e229a7c0420
guniism <gun16110@hotmail.com>
2025-01-23 00:20:25 +0700

    Add font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Kanit } from "next/font/google";
+
+const kanit = Kanit({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +31,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${kanit.className} antialiased`}
       >
         {children}
       </body>
