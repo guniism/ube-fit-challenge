e1bf156b22b92b1040e6a8384b85fa3c173f7c30
guniism <gun16110@hotmail.com>
2025-01-23 23:05:59 +0700

    Create signout button

--- app/components/signoutbtn.tsx
+++ app/components/signoutbtn.tsx
@@ -1,10 +1,19 @@
 "use client"
 import { signOut } from "next-auth/react"
-
+import Image from "next/image"
 export default function SignOutBtn(){
     return(
         <div>
-            <button onClick={() => signOut()}>ออกจากระบบ</button>
+            <button className="border-2 border-red w-full rounded-2xl h-14  text-red flex flex-row justify-center items-center" onClick={() => signOut()}>
+                <div className="mr-1 text-xl font-normal">ออกจากระบบ</div>
+                <Image
+                    height="24"
+                    width="24"
+                    src={`/assets/icons/logout-red.svg`}
+                    alt={`signout icon`}
+                    className="ml-1"
+                />
+            </button>
         </div>
     )
 }
