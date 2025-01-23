42882b1b92ad11dabd856e3efbc5c4c3b1306290
guniism <gun16110@hotmail.com>
2025-01-24 02:37:27 +0700

    Work on page

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -1,6 +1,14 @@
+"use client"
 import Navbar from "../components/navbar"
 import Header from "../components/header"
+import { useState  } from "react";
+import clsx from "clsx";
+import { useSession } from "next-auth/react";
+import { redirect } from 'next/navigation';
+
 export default function Page(){
+    const {data: session} = useSession();
+    if(!session) redirect("/login");
     const title = <div>
         <h1 className="text-3xl font-normal text-blue">
             บันทึก
@@ -8,9 +16,107 @@ export default function Page(){
             การออกกำลังกาย
         </h1>
     </div>;
+    const userEmail = session?.user?.email;
+    const [date, setDate] = useState(new Date().toISOString().split('T')[0]);
+    const [type, setType] = useState("");
+    const [quantity, setQuantity] = useState("");
+    const [imgurl, setImgurl] = useState("");
+    const [unit, setUnit] = useState("");
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
+    function handleType(type2: string){
+        if(type2 == "step"){
+            setUnit("ก้าว");
+        }
+        else if(type2 == "distance"){
+            setUnit("กิโลเมตร");
+        }
+        else if(type2 == "calorie"){
+            setUnit("แคลอรี่");
+        }
+    }
+    // console.log(date, type, quantity, imgurl, unit);
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        
+    
+        if(!date || !type || !quantity){
+          setError("กรุณากรอกข้อมูลให้ครบ");
+          return;
+        }
+    
+        try { 
+          const res = await fetch("http://localhost:3000/api/add", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                date, type, quantity, imgurl, userEmail
+            })
+          })
+    
+          if(res.ok){
+            const form = e.target;
+            setError("");
+            setSuccess("บันทึกสำเร็จ");
+            form.reset();
+          }
+          else{
+            console.log("Exercise add failed");
+          }
+    
+        } catch (error) {
+          console.log("Error during add exercise: ", error);
+        }
+      }
     return(
         <div>
             <Header style={title} profile={true}/>
+            <div className="px-10">            
+                <form onSubmit={handleSubmit} className='flex flex-col justify-evenly'>
+
+                <h3 className='ml-2 font-normal text-lg mb-1 mt-3'>วันที่ออกกำลังกาย</h3>
+                <input type="date" onChange={(e) => setDate(e.target.value)} defaultValue={new Date().toISOString().split('T')[0]} className='rounded-2xl p-3 block mx-auto bg-[#F5F5F5] w-full text-gray'></input>
+
+                <h3 className='ml-2 font-normal text-lg mb-1 mt-3'>ประเภทการออกกำลังกาย</h3>
+                <select id="type" onChange={(e) => {setType(e.target.value); handleType(e.target.value)}} name="type" className='rounded-2xl p-3 block mx-auto bg-[#F5F5F5] w-full text-gray'>
+                    <option value="" disabled selected>-- เลือกประเภท --</option>
+                    <option value="step">นับก้าว</option>
+                    <option value="distance">ระยะทาง เดิน/วิ่ง</option>
+                    <option value="calorie">แคลอรี่</option>
+                </select>
+
+                <h3 className='ml-2 font-normal text-lg mb-1 mt-3'>จำนวน</h3>
+                <div className="flex flex-row items-center">
+                    <input type="number" onChange={(e) => setQuantity(e.target.value)} className='rounded-2xl p-3 block mx-auto bg-[#F5F5F5] w-full text-gray' placeholder='กรุณากรอกจำนวน'></input>
+                    <p className={clsx("text-lg ",
+                        {
+                            "ml-5" : unit,
+                        }
+                    )}>
+                        {unit}
+                    </p>
+                </div>
+
+                <h3 className='ml-2 font-normal text-lg mb-1 mt-3'>ลิงค์รูปภาพ (ไม่จำเป็น)</h3>
+                <input type='text' onChange={(e) => setImgurl(e.target.value)} className='rounded-2xl p-3 block mx-auto bg-[#F5F5F5] w-full text-gray' placeholder='กรุณากรอกลิงค์รูปภาพ'></input>
+                
+                {error && (
+                    <div className='block rounded-2xl text-base text-red py-1 mx-auto w-full border border-red text-center mt-5'>{error}</div>
+                )}
+
+                {success && (
+                    <div className='block rounded-2xl text-base text-green py-1 mx-auto w-full border border-green text-center mt-5'>{success}</div>
+                )}
+
+                <button type='submit' className='block rounded-2xl text-xl mt-5 text-white py-4 mx-auto w-full bg-blue'>บันทึก</button>
+    
+                </form>
+            </div>
+
+
             <Navbar />
         </div>
     )
--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,12 +1,101 @@
+"use client"
+
 import Navbar from "../components/navbar"
 import Header from "../components/header"
+import { useSession } from "next-auth/react";
+import { redirect } from 'next/navigation';
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+interface Exercise {
+    date: string;
+    type: string;
+    quantity: number;
+    imgurl: string;
+  }
+
+function returnType(type: string, mode: number){
+    let text;
+    if(type == "step"){
+        text = (mode == 0) ? "นับก้าว" : "ก้าว";
+    }
+    else if(type == "distance"){
+        text = (mode == 0) ? "ระยะทาง การเดิน/วิ่ง," : "กิโลเมคร";
+    }
+    else if(type == "calorie"){
+        text = (mode == 0) ? "นับแคลอรี่" : "แคลอรี่";
+    }
+    return text;
+}
 export default function Page(){
+    const {data: session, status} = useSession();
+    if(!session) redirect("/login");
     const title = <div>
-    <h1 className="text-3xl font-normal text-blue">ประวัติการบันทึก</h1>
-</div>;
+        <h1 className="text-3xl font-normal text-blue">ประวัติการบันทึก</h1>
+    </div>;
+    const [error,setError] = useState("");
+    const [exercises,setExercises] = useState<Exercise[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+              const userEmail = session.user?.email;
+      
+              if (!userEmail) {
+                setError("No email found in the session.");
+                return;
+              }
+      
+              const res = await fetch(`/api/getexercise?userEmail=${userEmail}`);
+      
+              if (!res.ok) {
+                throw new Error("Failed to fetch data");
+              }
+      
+              const data = await res.json();
+              setExercises(data.exercises);
+            } catch (error: any) {
+              setError(error.message);
+            }
+        };
+        fetchData();
+    }, [session, status]);
+    
     return(
         <div>
             <Header style={title} profile={true}/>
+            <div className="px-10 mt-5">
+                <div>
+                    {error ? (<p>{error}</p>) : (<></>)}
+                    <div className="space-y-3">
+                        {exercises.length === 0 ? (
+                        <p className="text-center text-xl mt-10">ไม่มีประวัติการออกกำลังกาย</p>
+                        ) : (
+                        exercises.map((exercise: any) => (
+                            <div key={exercise._id} className="bg-lightgray rounded-2xl p-3 pl-4 items-center flex justify-between">
+                                <div className="">
+                                    
+                                    <h2 className=""><strong>ประเภท:</strong> {returnType(exercise.type, 0)}</h2>
+                                    <p className=""><strong>ปริมาณ:</strong> {exercise.quantity} {returnType(exercise.type, 1)}</p>
+                                    <p className="text-sm">บันทึกเมื่อวันที่: {new Date(exercise.date).toLocaleDateString()}</p>
+                                </div>
+                                {(exercise.imgurl == "") ? (<></>) : (
+                                    <Image
+                                    src={exercise.imgurl}
+                                    alt="exercise img"
+                                    width={500}
+                                    height={500}
+                                    className="w-20 h-20 object-cover border border-lightblue rounded-lg"
+                                    />
+                                )}
+  
+                                
+                            </div>
+                        ))
+                        )}
+                    </div>
+                </div>
+            </div>
             <Navbar />
         </div>
     )
